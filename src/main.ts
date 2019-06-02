4a8ea48c0e2c89ec52e4d2e630b05221df0e5173
t-kurihara <takuya.kurihara00@gmail.com>
2019-06-02 23:06:36 +0900

    [fix] remove Vuelidate.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import dayjs from 'dayjs'
 import 'dayjs/locale/ja'
-import Vuelidate from 'vuelidate'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
@@ -9,7 +8,6 @@ import store from './store'
 
 Vue.config.productionTip = false
 dayjs.locale('ja')
-Vue.use(Vuelidate)
 
 new Vue({
   router,
