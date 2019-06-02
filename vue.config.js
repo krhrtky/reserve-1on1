b46f9212d82045f2300b4d6816a3815c0b0e3d2d
t-kurihara <takuya.kurihara00@gmail.com>
2019-06-02 23:26:55 +0900

    [fix] assets path.

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,6 @@
-module.exports = {}
+module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/reserve-1on1/dist/'
+      : '/'
+}
