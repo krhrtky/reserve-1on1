3db320afd7c2bb0a40daabc260a45211df31a86f
t-kurihara <takuya.kurihara00@gmail.com>
2019-06-29 16:27:19 +0900

    [fix] format by lint.

--- src/model/InitialContent.ts
+++ src/model/InitialContent.ts
@@ -1,7 +1,7 @@
 export default {
-  to: "",
-  yourName: "",
+  to: '',
+  yourName: '',
   prefix: `以下の日程で1on1を実施します。\n
 お手数ですが会議室の確保をお願いします。`,
-  postfix: "以上、よろしくお願いします。"
-};
+  postfix: '以上、よろしくお願いします。'
+}
